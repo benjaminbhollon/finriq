fce8976635f8f1516333d81fffcea7a341b50b47
Zachary M <31627113+zmontgo@users.noreply.github.com>
2020-05-20 18:22:36 -0500

    Rewriting bot...

---
+++ bot.py
@@ -0,0 +1,22 @@
+import discord
+import os
+
+async def get_prefix(bot, message):
+    if not message.guild:
+        return commands.when_mentioned_or('!')(bot, message)
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print('We have logged in as {0.user}'.format(client))
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('$hello'):
+        await message.channel.send('Hello!')
+
+client.run(os.environ.get('BOT_TOKEN'))
