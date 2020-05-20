5930124aad0ea1443fb48c483550e10246487ee6
Zachary M <31627113+zmontgo@users.noreply.github.com>
2020-05-20 18:12:06 -0500

    Thing

--- bot.js
+++ bot.js
@@ -55,7 +55,12 @@ function isEmpty(obj) {
 }
 
 function shuffle(array) {
-  array.sort(() => Math.random() - 0.5);
+  for(let i = array.length — 1; i > 0; i--){
+    const j = Math.floor(Math.random() * i)
+    const temp = array[i]
+    array[i] = array[j]
+    array[j] = temp
+  }
 }
 
 function addSummaryMessage(id) {
