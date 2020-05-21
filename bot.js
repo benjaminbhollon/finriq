cf7521d6b1c24134429594e643a9d988c2f21ead
Zachary M <31627113+zmontgo@users.noreply.github.com>
2020-05-21 07:30:57 -0500

    More testing

--- bot.js
+++ bot.js
@@ -1,9 +1,6 @@
 const path = require('path');
 const liora = require('liora');
 
-// If setConfigDirectory is not called, Liora will use the default at ~/.liora-bot/
-liora.setConfigDirectory('path/to/your/config/directory');
-
 // Call addModuleSource with the absolute path to a directory to make that directory a module source
 // Your custom modules will go in this folder and it is possible to add multiple module sources
 // Liora will still load its internal modules in addition to yours
