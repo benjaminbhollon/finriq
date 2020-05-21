4097d29958dfd14bad319031a2017986fd4bf0b1
Zachary M <31627113+zmontgo@users.noreply.github.com>
2020-05-21 09:56:56 -0500

    Rewriting bot

--- bot.js
+++ bot.js
@@ -1,13 +1,45 @@
-const path = require('path');
-const liora = require('liora');
+// Load dependencies
+const fs = require('fs');
+const Discord = require('discord.js');
+const config = require('./config.json');
+const connect = require('./databaseFiles/connect.js');
 
-// Call addModuleSource with the absolute path to a directory to make that directory a module source
-// Your custom modules will go in this folder and it is possible to add multiple module sources
-// Liora will still load its internal modules in addition to yours
-liora.addModuleSource(path.join(__dirname, 'modules'));
+const client = new Discord.Client();
 
-// Start the bot
-liora.load(() => {
-	// Generate documentation for all loaded modules
-	liora.generateDocs('COMMANDS.md');
+fs.readdir('./events/', (err, files) => {
+	if (err) return console.error(err);
+	const jsfile = files.filter((f) => f.split('.').pop() === 'js');
+	if (jsfile.length <= 0) {
+		return console.log('No errors have been loaded!');
+	}
+	jsfile.forEach((file) => {
+		const event = require(`./events/${file}`);
+		const eventName = file.split('.')[0];
+		client.on(eventName, event.bind(null, client));
+	});
 });
+
+client.commands = new Discord.Collection();
+client.aliases = new Discord.Collection();
+
+fs.readdir('./commands/', (err, files) => {
+	if (err) console.error(err);
+	const jsfile = files
+		.filter((t) => !t.includes('.test.'))
+		.filter((f) => f.split('.').pop() === 'js');
+	if (jsfile.length <= 0) {
+		return console.log('No commands have been loaded!');
+	}
+	jsfile.forEach((f) => {
+		const pull = require(`./commands/${f}`);
+		client.commands.set(pull.config.name, pull);
+		pull.config.aliases.forEach((alias) => {
+			client.aliases.set(alias, pull.config.name);
+		});
+	});
+});
+
+// Connect to given database
+connect.instantiateConnection();
+
+client.login(config.token);
