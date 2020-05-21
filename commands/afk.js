21ae87c7ca1f836d0f2e484b8e2f76aec221b082
Zachary M <31627113+zmontgo@users.noreply.github.com>
2020-05-21 12:02:18 -0500

    Bettered AFK Message

--- commands/afk.js
+++ commands/afk.js
@@ -13,7 +13,7 @@ module.exports.execute = async (client, message, args) => {
 			user: sender.id,
 			cooldown: 300000
 		}).then(() => {
-			sender.send('I have marked you as AFK. Anyone who pings you will be notified you are away.');
+			return await message.channel.send('I have marked you as AFK. Anyone who pings you will be notified you are away.');
 		}).catch(err => {
 			if (err.name == 'SequelizeUniqueConstraintError') {
 				return;
