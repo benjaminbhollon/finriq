d88bdc0b0a91fa9e5be67c1b0422cac72ddcb4ac
Zachary M <31627113+zmontgo@users.noreply.github.com>
2020-05-21 13:26:31 -0500

    Fixed tea and coffee

--- commands/coffee.js
+++ commands/coffee.js
@@ -2,7 +2,7 @@ module.exports.execute = async (client, message, args) => {
   return await message.channel.send(Math.random() < 0.5 ? ":coffee:" : ":coffin:");
 };
 
-module.exports.exports = {
+module.exports.execute = {
   name: 'coffee',
   aliases: ['cappuccino', 'coffin', 'croulette'],
   description: 'I will choose either :coffee: or :coffin:. (Finriq version of Russian Roulette)',
