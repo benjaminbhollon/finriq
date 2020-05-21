7cfd6206c7180ad29683c1292054753fb01ec880
Zachary M <31627113+zmontgo@users.noreply.github.com>
2020-05-21 13:46:17 -0500

    Added shoot command

--- commands/shoot.js
+++ commands/shoot.js
@@ -1,10 +1,34 @@
 module.exports.execute = async (client, message, args) => {
-  return await message.channel.send("Hey there!");
+  function getRandomInt(max) {
+    return Math.floor(Math.random() * Math.floor(max));
+  }
+
+  if (getRandomInt(100) == 99) {
+    if (parseInt(args[0])) {
+      return await message.channel.send("_Shoots <@" + args[0] + "> violently._");
+    } else {
+      function setName(input) {
+        name = input;
+      }
+      var name = args.join(" ");
+      //Replace with mention if possible
+      message.channel.members.forEach(member => {
+        if (member.displayName.toLowerCase().indexOf(name.toLowerCase()) != -1 || member.user.username.toLowerCase().indexOf(name.toLowerCase()) != -1) name = "<@" + member.id + ">";
+      });
+      if (name != "@everyone") {
+        return await message.channel.send("_Genocide._");
+      } else {
+        return await message.channel.send("_Shoots <@" + name + "> violently._");
+      }
+    }
+  } else {
+    return await message.channel.send("_Violence is never the answer. Do... you need a hug?_");
+  }
 };
 
 module.exports.config = {
-  name: 'hello',
-  aliases: ['hey', 'greetings'],
-  description: 'Says hello. Use to test if bot is online.',
-  usage: ['hello']
+  name: 'shoot',
+  aliases: ['gun'],
+  description: 'Shoots *user*.',
+  usage: ['shoot user']
 };
