06c2739814a6b45ff66492f3159d20222fc37bdd
Zachary M <31627113+zmontgo@users.noreply.github.com>
2020-05-21 11:49:23 -0500

    Testing

--- commands/help.js
+++ commands/help.js
@@ -3,6 +3,7 @@ const Discord = require('discord.js');
 let prefix;
 console.log(require('../config.json').prefix);
 if (fs.existsSync('../config.json')) {
+	console.log("True");
 	prefix = require('../config.json').prefix;
 } else {
 	prefix = '!';
