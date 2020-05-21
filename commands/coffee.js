9602620c3ee7c9b1a8be47dec536658d27a286a7
Zachary M <31627113+zmontgo@users.noreply.github.com>
2020-05-21 13:27:44 -0500

    Incorrect name

--- commands/coffee.js
+++ commands/coffee.js
@@ -2,7 +2,7 @@ module.exports.execute = async (client, message, args) => {
   return await message.channel.send(Math.random() < 0.5 ? ":coffee:" : ":coffin:");
 };
 
-module.exports.execute = {
+module.exports.config = {
   name: 'coffee',
   aliases: ['cappuccino', 'coffin', 'croulette'],
   description: 'I will choose either :coffee: or :coffin:. (Finriq version of Russian Roulette)',
--- commands/tea.js
+++ commands/tea.js
@@ -2,7 +2,7 @@ module.exports.execute = async (client, message, args) => {
   return await message.channel.send(Math.random() < 0.5 ? ":tea:" : ":deciduous_tree:");
 };
 
-module.exports.execute = {
+module.exports.config = {
 	name: 'tea',
 	aliases: ['chai', 'tree', 'troulette'],
 	description: 'I will choose either :tea: or :deciduous_tree:. (Tea-drinker\'s version of the coffee command)',
