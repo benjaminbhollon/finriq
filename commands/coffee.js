e7e89a0b59ba5668eae8a907a657d375a6526fec
Zachary M <31627113+zmontgo@users.noreply.github.com>
2020-05-21 13:25:20 -0500

    Added tea and coffee commands

---
+++ commands/coffee.js
@@ -0,0 +1,10 @@
+module.exports.execute = async (client, message, args) => {
+  return await message.channel.send(Math.random() < 0.5 ? ":coffee:" : ":coffin:");
+};
+
+module.exports.exports = {
+  name: 'coffee',
+  aliases: ['cappuccino', 'coffin', 'croulette'],
+  description: 'I will choose either :coffee: or :coffin:. (Finriq version of Russian Roulette)',
+  usage: ['coffee']
+};
---
+++ commands/tea.js
@@ -0,0 +1,10 @@
+module.exports.execute = async (client, message, args) => {
+  return await message.channel.send(Math.random() < 0.5 ? ":tea:" : ":deciduous_tree:");
+};
+
+module.exports.tea = {
+	name: 'tea',
+	aliases: ['chai', 'tree', 'troulette'],
+	description: 'I will choose either :tea: or :deciduous_tree:. (Tea-drinker\'s version of the coffee command)',
+	usage: ['tea']
+};
