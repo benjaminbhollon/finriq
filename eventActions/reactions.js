39a0348e17334fc33223795dc36e939545bb8fce
Zachary M <31627113+zmontgo@users.noreply.github.com>
2020-05-22 09:33:28 -0500

    Testing

--- eventActions/reactions.js
+++ eventActions/reactions.js
@@ -2,12 +2,13 @@ const reactions = JSON.parse(require("../config.json").channelReacts);
 
 class reactionCheckAction {
   static async checkIfCorrect(message) {
-    for(var i = 0; i < reactions.length; i++) {
+    console.log(reactions);
+    /*for(var i = 0; i < reactions.length; i++) {
       var obj = reactions[i];
   
       console.log(obj.channel);
       console.log(obj.reaction);
-    }
+    }*/
   }
 }
 
