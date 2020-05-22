bc97b1c689bde47d672f1aeecf57f34d81457ab9
Benjamin <bhollon@duti.site>
2020-05-22 08:42:36 +0800

    Patch hug module

--- commands/hug.js
+++ commands/hug.js
@@ -1,6 +1,8 @@
 module.exports.execute = async (client, message, args) => {
+  message.delete();
+  message.guild.channels.find(channel => channel.name == 'logs').send("Message by " + message.author.tag + " deleted in " + message.channel + ":\n\"" + message.content + "\"");
   if (parseInt(args[0])) {
-    return await message.channel.send(`_Hugs${args[0]}._\n_Don't worry, it'll be alright._`);
+    return await message.channel.send(`_Hugs <@${args[0]}>._\n_Don't worry, it'll be alright._`);
   } else {
     var name = args.join(" ");
     //Replace with mention if possible
@@ -8,7 +10,7 @@ module.exports.execute = async (client, message, args) => {
       if (member.displayName.toLowerCase().indexOf(name.toLowerCase()) != -1 || member.user.username.toLowerCase().indexOf(name.toLowerCase()) != -1) name = "<@" + member.id + ">";
     });
     if (name != "@everyone") {
-      return await message.channel.send(`_Hugs${name}._\n_Don't worry, it'll be alright._`);
+      return await message.channel.send(`_Hugs ${name}._\n_Don't worry, it'll be alright._`);
     } else {
       return await message.channel.send("_Hugs the entire server._\n_Don't worry, it'll be alright._");
     }
