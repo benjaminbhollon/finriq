5d149aa9c86f7895a2ba471f065b30da0dc8115a
Zachary M <31627113+zmontgo@users.noreply.github.com>
2020-05-27 15:01:40 -0400

    Hopefully?

--- commands/agree.js
+++ commands/agree.js
@@ -3,9 +3,13 @@ const config = require('../config.json');
 
 
 module.exports.execute = async (client, message, args) => {
-	return await message.channel.send("Hey there!");
+  return await message.channel.send("Hey there!");
 };
 
 module.exports.config = {
-	name: 'agree'
+	name: 'afk',
+	aliases: [],
+	module: null,
+	description: null,
+	usage: null
 };
