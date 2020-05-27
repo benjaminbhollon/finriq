6e7df783e2617c9eaf48fa3de62ba9af6d476e1b
Zachary M <31627113+zmontgo@users.noreply.github.com>
2020-05-27 10:21:37 -0400

    Fixed @everyone pings

--- commands/hug.js
+++ commands/hug.js
@@ -5,7 +5,7 @@ module.exports.execute = async (client, message, args) => {	try {
   }
   
   try {
-    message.guild.channels.find(channel => channel.name == 'logs').send("Message by " + message.author.tag + " deleted in " + message.channel + ":\n\"" + message.content + "\"");
+    message.guild.channels.find(channel => channel.name == 'logs').send("Message by " + message.author.tag.replace("@everyone", "@.everyone") + " deleted in " + message.channel.replace("@everyone", "@.everyone") + ":\n\"" + message.content.replace("@everyone", "@.everyone") + "\"");
   }
   catch (err) {
     console.log(err);
