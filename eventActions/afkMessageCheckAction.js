392e2078a9bc9485875b15995c95bafd9ba4d73b
Zachary M <31627113+zmontgo@users.noreply.github.com>
2020-05-27 09:29:36 -0400

    Fix timestamp issue

--- eventActions/afkMessageCheckAction.js
+++ eventActions/afkMessageCheckAction.js
@@ -55,6 +55,7 @@ class afkMessageCheckAction {
 		const user = message.author;
 
 		function timedifference(timestamp1, timestamp2) {
+			var timestamp1 = new Date(timestamp1); // timestamp1 MUST be from the database
 			var difference = timestamp1.getTime() - timestamp2.getTime();
 
 			var difference = Math.floor(difference/1000);
