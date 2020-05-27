37194d650d834cf24a7d16491d5db3e3e86fac3b
Zachary M <31627113+zmontgo@users.noreply.github.com>
2020-05-27 10:42:03 -0400

    Update logs

--- commands/hug.js
+++ commands/hug.js
@@ -1,12 +1,13 @@
 module.exports.execute = async (client, message, args) => {
   try {
-    message.delete();
 
     let logMessage = new Discord.RichEmbed()
 				.setColor('#750384')
 				.setTitle(`\`.hug\` command deleted in ${message.channel}.`)
 			helpMessage.addField('User:', message.author.tag);
-			helpMessage.addField('Message:', message.content);
+      helpMessage.addField('Message:', message.content);
+      
+    message.delete();
 
 			try {
 				message.channel.send(logMessage);
@@ -17,7 +18,7 @@ module.exports.execute = async (client, message, args) => {
   } catch(err) {
     console.log("Delete error" + err);
   }
-  
+
   if (parseInt(args[0])) {
     return await message.channel.send(`_Hugs <@${args[0]}>._\n_Don't worry, it'll be alright._`);
   } else {
