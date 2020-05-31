e4bf0af478c3dbc93a2c25848b354c263a194630
Zachary M <31627113+zmontgo@users.noreply.github.com>
2020-05-31 09:59:40 -0400

    Customized welcome message

--- eventActions/welcomeActions.js
+++ eventActions/welcomeActions.js
@@ -7,23 +7,24 @@ class tosActions {
             && reaction._emoji.name === config.emotes.acceptTOS) {
 			reaction.message.guild.fetchMember(user.id).then(guildMember => {
         // Send welcome message to the welcome channel
-        client.channels.get(config.channels.welcome).send(`🎉 **A new member has arrived!** 🎉\nWelcome to The Bookery <@${user.id}>!`)
+        client.channels.get(config.channels.welcome).send(`🎉 **A new member has arrived, <@&693517619457818634>!** 🎉\nWelcome to The Bookery <@${user.id}>!`)
           .then(message => {
             message.react(config.emotes.wave);
           });
-				user.send(`**Welcome to The Bookery!** :blush:
-To begin, say hello in <#382364344731828226> and read up in <#384040181763670026> for our step by step guide.
-Lastly, to learn more visit us any time at: <https://knightsofacademia.org>.
-**Ask me questions, tell me to do stuff.**
-I am your all in one assistant, designed to help and cater to your needs. It's nice to meet you. 
-Here are a few things you can ask me:
--!info [term]
--!raid
--!help
--!highlight
--!clans/!apply
--!invite [koa][koai][jesters]
--!choose [choice 1],[choice 2]`);
+        user.send(`\`\`\`fix
+        Welcome to The Bookery!\`\`\`
+To begin, say hello in <#693498873083330654> and read up in <#> for our step by step guide.
+
+**Quick Start Guide**
+• You can grab some roles in <#693563108077076490>.
+• Introduce yourself with the template pinned in <#711269048591056916>.
+• To learn more about the server, check out <#693500724704837653>.
+
+**Reading Groups**
+We are a reading server, and we host multiple reading groups! Learn more in the info channel, and join in the self roles channel! Have fun!
+
+**About Me**
+I'm Finriq, a custom bot made for The Bookery! I am always growing. Use \`.help\` in <#693561975887888444> for more on what I can do.`);
 			});
 		}
 	}
