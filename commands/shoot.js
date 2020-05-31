2c2a97768691a4b0097ef3f9f16950dce54e10a6
Zachary M <31627113+zmontgo@users.noreply.github.com>
2020-05-31 17:34:19 -0400

    Updated shoot command

--- commands/shoot.js
+++ commands/shoot.js
@@ -2,13 +2,34 @@ const Discord = require('discord.js');
 const logschannel = require('../config.json').channels.logs;
 
 module.exports.execute = async (client, message, args) => {
+  try {
+
+    let logMessage = new Discord.RichEmbed()
+				.setColor('#750384')
+				.setTitle(`\`.hug\` command deleted`)
+			logMessage.addField('User:', message.author.tag);
+      logMessage.addField('Message:', message.content);
+      logMessage.addField('Channel:', message.channel);
+      
+    message.delete();
+
+			try {
+				message.guild.channels.get(logschannel).send(logMessage);
+			}
+			catch(err) {
+				console.log(err);
+			}
+  } catch(err) {
+    console.log("Delete error" + err);
+  }
+
   function getRandomInt(max) {
     return Math.floor(Math.random() * Math.floor(max));
   }
 
   if (getRandomInt(100) == 99) {
     if (parseInt(args[0])) {
-      return await message.channel.send(`_Shoots <@${args[0]}> violently._`);
+      return await message.channel.send(`_${message.author.tag} shoots <@${args[0]}> violently._`);
     } else {
       var name = args.join(" ");
       //Replace with mention if possible
@@ -16,13 +37,13 @@ module.exports.execute = async (client, message, args) => {
         if (member.displayName.toLowerCase().indexOf(name.toLowerCase()) != -1 || member.user.username.toLowerCase().indexOf(name.toLowerCase()) != -1) name = "<@" + member.id + ">";
       });
       if (name != "@everyone") {
-        return await message.channel.send(`_Shoots ${name} violently._`);
+        return await message.channel.send(`_${message.author.tag} shoots ${name} violently._`);
       } else {
-        return await message.channel.send("_Genocide._");
+        return await message.channel.send(`_${message.author.tag} commits genocide._`);
       }
     }
   } else {
-    return await message.channel.send('Violence is never the answer. Do... you need a `.hug`?');
+    return await message.channel.send(`Violence is never the answer, ${message.author.tag}. Do... you need a \`.hug\`?`);
   }
 };
 
