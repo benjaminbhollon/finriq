a065454a4c524126c710e20c3e6b114764b9dbc7
Zachary M <31627113+zmontgo@users.noreply.github.com>
2020-06-11 11:50:27 -0400

    Fixed grammar issue

--- eventActions/afkMessageCheckAction.js
+++ eventActions/afkMessageCheckAction.js
@@ -99,23 +99,33 @@ class afkMessageCheckAction {
 		
 			var interval = Math.floor(seconds / 31536000);
 		
-			if (interval >= 1) {
+			if (interval == 1) {
+				return interval + " year";
+			} else if (interval >= 1) {
 				return interval + " years";
 			}
 			interval = Math.floor(seconds / 2592000);
-			if (interval >= 1) {
+			if (interval == 1) {
+				return interval + " month";
+			} else if (interval >= 1) {
 				return interval + " months";
 			}
 			interval = Math.floor(seconds / 86400);
-			if (interval >= 1) {
+			if (interval == 1) {
+				return interval + " day";
+			} else if (interval >= 1) {
 				return interval + " days";
 			}
 			interval = Math.floor(seconds / 3600);
-			if (interval >= 1) {
+			if (interval == 1) {
+				return interval + " hour";
+			} else if (interval >= 1) {
 				return interval + " hours";
 			}
 			interval = Math.floor(seconds / 60);
-			if (interval >= 1) {
+			if (interval == 1) {
+				return interval + " minute";
+			} else if (interval >= 1) {
 				return interval + " minutes";
 			}
 			return Math.floor(seconds) + " seconds";
